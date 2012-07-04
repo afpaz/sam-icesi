a392615379f8c42cb44a7bb27758256bff19a0e2
Juan Medina <juan.medina91@gmail.com>
2012-07-04 09:22:48 -0500

    Lista de Chequeo de Eventos

--- SAM-WAR/src/main/java/co/edu/icesi/sam/client/controller/DTEvent.java
+++ SAM-WAR/src/main/java/co/edu/icesi/sam/client/controller/DTEvent.java
@@ -5,6 +5,29 @@ import com.extjs.gxt.ui.client.mvc.AppEvent;
 
 public class DTEvent
 {      
+    public final static EventType SELECCIONAR_CURSO = new EventType( );
+    public final static EventType AGREGAR_OBJ_GENERAL = new EventType( );
+    public final static EventType EDITAR_OBJ_GENERAL = new EventType( );
+    public final static EventType AGREGAR_OBJ_TERMINAL = new EventType( );
+    public final static EventType EDITAR_OBJ_TERMINAL = new EventType( );
+    public final static EventType AGREGAR_UNIDAD = new EventType( );
+    public final static EventType EDITAR_UNIDAD = new EventType( );
+    public final static EventType AGREGAR_META_TERMINAL = new EventType( );
+    public final static EventType ELIMINAR_META_TERMINAL = new EventType( );
+    public final static EventType AGREGAR_OBJ_ESPECIFICO = new EventType( );
+    public final static EventType EDITAR_OBJ_ESPECIFICO = new EventType( );
+    public final static EventType AGREGAR_SABER = new EventType( );
+    public final static EventType EDITAR_SABER = new EventType( );
+    public final static EventType AGREGAR_MATERIAL = new EventType( );
+    public final static EventType EDITAR_MATERIAL = new EventType( );
+    public final static EventType AGREGAR_RECURSO = new EventType( );
+    public final static EventType AGREGAR_SESION = new EventType( );
+    public final static EventType EDITAR_SESION = new EventType( );
+    public final static EventType AGREGAR_TRABAJO_ASIGNADO = new EventType( );
+    public final static EventType EDITAR_TRABAJO_ASIGNADO = new EventType( );
+    public final static EventType AGREGAR_RECURSO_ASIGNADO = new EventType( );
+    public final static EventType EDITAR_RECURSO_ASIGNADO = new EventType( );
+    
     public static AppEvent getAppEvent(EventType ev)
     {
         return new AppEvent(ev);
--- SAM-WAR/src/main/java/co/edu/icesi/sam/client/controller/DTViewController.java
+++ SAM-WAR/src/main/java/co/edu/icesi/sam/client/controller/DTViewController.java
@@ -7,13 +7,125 @@ public class DTViewController extends Controller
 {    
     public DTViewController( )
     {
-        // TODO Auto-generated constructor stub
+        registerEventTypes( DTEvent.AGREGAR_MATERIAL );
+        registerEventTypes( DTEvent.AGREGAR_META_TERMINAL );
+        registerEventTypes( DTEvent.AGREGAR_OBJ_ESPECIFICO );
+        registerEventTypes( DTEvent.AGREGAR_OBJ_GENERAL );
+        registerEventTypes( DTEvent.AGREGAR_OBJ_TERMINAL );
+        registerEventTypes( DTEvent.AGREGAR_RECURSO );
+        registerEventTypes( DTEvent.AGREGAR_RECURSO_ASIGNADO );
+        registerEventTypes( DTEvent.AGREGAR_SABER );
+        registerEventTypes( DTEvent.AGREGAR_SESION );
+        registerEventTypes( DTEvent.AGREGAR_TRABAJO_ASIGNADO );
+        registerEventTypes( DTEvent.AGREGAR_UNIDAD );
+        registerEventTypes( DTEvent.EDITAR_MATERIAL );
+        registerEventTypes( DTEvent.EDITAR_OBJ_ESPECIFICO );
+        registerEventTypes( DTEvent.EDITAR_OBJ_GENERAL );
+        registerEventTypes( DTEvent.EDITAR_OBJ_TERMINAL );
+        registerEventTypes( DTEvent.EDITAR_RECURSO_ASIGNADO );
+        registerEventTypes( DTEvent.EDITAR_SABER );
+        registerEventTypes( DTEvent.EDITAR_SESION );
+        registerEventTypes( DTEvent.EDITAR_TRABAJO_ASIGNADO );
+        registerEventTypes( DTEvent.EDITAR_UNIDAD );  
+        registerEventTypes( DTEvent.ELIMINAR_META_TERMINAL );
+        registerEventTypes( DTEvent.SELECCIONAR_CURSO );
     }
     
     @Override
     public void handleEvent( AppEvent event )
     {
-        // TODO Auto-generated method stub
+        if(event.getType( ).equals( DTEvent.AGREGAR_MATERIAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_META_TERMINAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_OBJ_ESPECIFICO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_OBJ_GENERAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_META_TERMINAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_OBJ_TERMINAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_RECURSO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_RECURSO_ASIGNADO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_SABER ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_SESION ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_TRABAJO_ASIGNADO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.AGREGAR_UNIDAD ))
+        {
+            
+        }
+        if(event.getType( ).equals( DTEvent.EDITAR_MATERIAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_OBJ_ESPECIFICO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_OBJ_GENERAL ))
+        {
+            
+        }        
+        else if(event.getType( ).equals( DTEvent.EDITAR_OBJ_TERMINAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_RECURSO_ASIGNADO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_SABER ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_SESION ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_TRABAJO_ASIGNADO ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.EDITAR_UNIDAD ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.ELIMINAR_META_TERMINAL ))
+        {
+            
+        }
+        else if(event.getType( ).equals( DTEvent.SELECCIONAR_CURSO ))
+        {
+            
+        }
     }
 
 }
