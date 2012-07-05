61176d19355bf433b6a17d3d5708a537ca7aecc4
Juan Medina <juan.medina91@gmail.com>
2012-07-04 22:07:26 -0500

    CursoModel

---
+++ SAM-WAR/src/main/java/co/edu/icesi/sam/client/model/CursoModel.java
@@ -0,0 +1,64 @@
+package co.edu.icesi.sam.client.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import co.edu.icesi.sam.bo.CursoBO;
+
+import com.extjs.gxt.ui.client.data.BaseModel;
+
+public class CursoModel extends BaseModel implements Serializable
+{
+    
+    public CursoModel()
+    {
+        
+    }
+    
+    public CursoModel(int id, String nombre, String codigo)
+    {
+        set( "id", id );
+        set("nombre", nombre);
+        set("codigo", codigo);
+    }
+
+    public int getId( )
+    {
+        return (Integer) get("id");
+    }
+
+    public String getCodigo( )
+    {
+        return (String) get("codigo");
+    }
+
+    public String getNombre( )
+    {
+        return (String) get( "nombre" );
+    }    
+
+    public void setId( int id )
+    {
+        set("id", id);
+    }
+
+    public void setCodigo( String codigo )
+    {
+        set("codigo", codigo);
+    }
+
+    public void setNombre( String nombre )
+    {
+        set( "nombre", nombre );
+    }       
+    
+    public CursoModel toModelFromBO(CursoBO c)
+    {
+        CursoModel cModel = new CursoModel( );
+        cModel.setId( c.getId( ));
+        cModel.setCodigo( c.getCodigo( ) );
+        cModel.setNombre( c.getNombre( ) );
+        
+        return cModel;
+    }
+}
