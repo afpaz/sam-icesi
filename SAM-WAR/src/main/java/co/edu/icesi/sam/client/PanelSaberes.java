75ea3161f07166ab96ffd73ddd1f1ace5b4305b7
Diego Rojas <diegoarmando9066@gmail.com>
2012-07-05 11:22:56 -0500

    Tablas

--- SAM-WAR/src/main/java/co/edu/icesi/sam/client/PanelSaberes.java
+++ SAM-WAR/src/main/java/co/edu/icesi/sam/client/PanelSaberes.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 import org.apache.xerces.impl.dtd.models.CMUniOp;
 
+import co.edu.icesi.sam.client.controller.DTEvent;
 import co.edu.icesi.sam.client.model.CursoModel;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.data.BaseModel;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.GridEvent;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.mvc.Dispatcher;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
@@ -80,52 +86,93 @@ public class PanelSaberes extends LayoutContainer {
 
 
 
-//		//CARGA LA TABLA DE SABERES
-//		List<ColumnConfig> configSaberes = new ArrayList<ColumnConfig>();  
-//
-//		ColumnConfig columnSaberes = new ColumnConfig();    
-//		columnSaberes.setId("id");    
-//		columnSaberes.setHeader("Identificacion");    
-//		columnSaberes.setWidth(80);    
-//		configSaberes.add(columnSaberes);  
-//
-//		columnSaberes = new ColumnConfig("nombre", "Nombre", 100);    
-//		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
-//		configSaberes.add(columnSaberes);  
-//
-//		columnSaberes = new ColumnConfig("tipo", "Tipo", 200);    
-//		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
-//		configSaberes.add(columnSaberes);  
-//
-//		columnSaberes = new ColumnConfig("contenido", "Contenido", 200);    
-//		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
-//		configSaberes.add(columnSaberes);  
-//
-//		ListStore<CursoModel> listaSaberes = new ListStore<CursoModel>();    
-//		//employeeList.add(TestData.getEmployees()); 
-//
-//		cmSaberes = new ColumnModel(configSaberes);  
-//		gridSaberes = new Grid<CursoModel>(listaSaberes, cmSaberes);   
-//		gridSaberes.setStyleAttribute("borderTop", "none");   
-//		gridSaberes.setAutoExpandColumn("name");   
-//		gridSaberes.setBorders(true);   
-//		gridSaberes.setStripeRows(true); 
-//
-//		ContentPanel cpSaberes = new ContentPanel();    
-//		cpSaberes.setBodyBorder(false);    
-//		cpSaberes.setHeading("Lista de Saberes:");    
-//		cpSaberes.setButtonAlign(HorizontalAlignment.CENTER);    
-//		cpSaberes.setLayout(new FitLayout());    
-//		cpSaberes.setSize(450, 116);   
-//		cpSaberes.add(gridSaberes);   
-//
-//		add(cpSaberes, new AbsoluteData(36, 142));
+		//CARGA LA TABLA DE SABERES
+		List<ColumnConfig> configSaberes = new ArrayList<ColumnConfig>();  
+
+		ColumnConfig columnSaberes = new ColumnConfig();    
+		columnSaberes.setId("id");    
+		columnSaberes.setHeader("Identificacion");    
+		columnSaberes.setWidth(80);    
+		configSaberes.add(columnSaberes);  
+
+		columnSaberes = new ColumnConfig("nombre", "Nombre", 100);    
+		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
+		configSaberes.add(columnSaberes);  
+
+		columnSaberes = new ColumnConfig("tipo", "Tipo", 200);    
+		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
+		configSaberes.add(columnSaberes);  
+
+		columnSaberes = new ColumnConfig("contenido", "Contenido", 200);    
+		columnSaberes.setAlignment(HorizontalAlignment.LEFT);    
+		configSaberes.add(columnSaberes);  
+
+		ListStore<CursoModel> listaSaberes = new ListStore<CursoModel>();    
+		//employeeList.add(TestData.getEmployees()); 
+
+		cmSaberes = new ColumnModel(configSaberes);  
+		gridSaberes = new Grid<CursoModel>(listaSaberes, cmSaberes);   
+		gridSaberes.setStyleAttribute("borderTop", "none");   
+		gridSaberes.setAutoExpandColumn("name");   
+		gridSaberes.setBorders(true);   
+		gridSaberes.setStripeRows(true); 
+
+		ContentPanel cpSaberes = new ContentPanel();    
+		cpSaberes.setBodyBorder(false);    
+		cpSaberes.setHeading("Lista de Saberes:");    
+		cpSaberes.setButtonAlign(HorizontalAlignment.CENTER);    
+		cpSaberes.setLayout(new FitLayout());    
+		cpSaberes.setSize(450, 116);   
+		cpSaberes.add(gridSaberes);   
+
+		add(cpSaberes, new AbsoluteData(36, 142));
 
 	}
 
 	public void actualizarTablaSaberes(ListStore<CursoModel> listaUnidades){
 
 		gridSaberes.reconfigure(listaUnidades, cmSaberes);
+
+	}
+
+	public void eventoSeleccionarSaber( )
+	{
+		gridSaberes.addListener( Events.OnClick, new Listener<GridEvent<BaseModel>>( )
+				{
+			public void handleEvent( GridEvent<BaseModel> be )
+			{
+				CursoModel cursoModel = (CursoModel) be.getGrid( ).getSelectionModel( ).getSelectedItem( );
+				Dispatcher.forwardEvent( DTEvent.SELECCIONAR_CURSO, cursoModel );
+			}
+				} );
+
+	}
+
+	public void eventoSeleccionarObj( )
+	{
+		gridObjEspecifico.addListener( Events.OnClick, new Listener<GridEvent<BaseModel>>( )
+				{
+			public void handleEvent( GridEvent<BaseModel> be )
+			{
+
+			}
+				} );
+
+	}
+
+	public void cargarSaberes(){
+
+		gridSaberes.addListener( Events.OnClick, new Listener<GridEvent<BaseModel>>( )
+				{
+			public void handleEvent( GridEvent<BaseModel> be )
+			{
+
+			}
+				} );
+
+	}
+
+	public void cargarObjEspecificos(){
 
 	}
 
